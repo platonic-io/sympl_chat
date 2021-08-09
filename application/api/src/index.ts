2a4832cff9d6cf91514ad786847abc9562552f2d
David Bono <david.bono@symbiont.io>
2021-08-09 16:49:48 -0400

    added routes/logic for middleware

--- application/api/src/index.ts
+++ application/api/src/index.ts
@@ -3,7 +3,10 @@ import * as contracts from '../@assembly/contract';
 import { Chat } from '../@assembly/contract';
 import Koa, {Context} from 'koa';
 import Router from 'koa-router';
-import { exit } from 'process';
+import mount from 'koa-mount';
+
+//routes
+import * as users from './routes/users';
 
 try {    
     var CONFIG = JSON.parse(fs.readFileSync('network_config.json', 'utf-8'));
@@ -17,27 +20,16 @@ const nodeClient = networkClient.nodeClients[0];
 const chat : Chat = new contracts["Chat"](networkClient);
 
 const app : Koa = new Koa();
-const app_router : Router = new Router();
 
-app_router.get('/create_ka', async (ctx : Context) => {
-    ctx.body = await nodeClient.registerKeyAlias();
-})
+const api : Koa = new Koa();
+const api_router : Router = new Router();
 
-app_router.get("/list_ka", async (ctx: Context) => {
-    ctx.body = await nodeClient.listKeyAliases();
-})
+api_router.get("/get_users", users.getUsers);
+api_router.post("/create_user", users.createUser);
 
-app_router.get("/create_room/:caller/:room_name", async (ctx: Context) => {
-    ctx.body = await chat.createRoom(ctx.params.caller, ctx.params.room_name);
-})
+api.use(api_router.middleware())
 
-app_router.get("/", async (ctx: Context, next: any) => {
-    ctx.body = "<a href='/create_ka'>Create Ka </a><br><a href='/list_ka'>List Ka</a>"
-    return next();
-})
-
-
-app.use(app_router.middleware())
+app.use(mount('/api', api))
 
 app.listen(8080);   
 
---
+++ application/api/src/user-manager.ts
@@ -0,0 +1,70 @@
+import { create } from 'domain';
+import * as fs from 'fs';
+import {networkClient, chat} from './assembly-wrapper';
+
+const users_db_location = 'users.json'
+
+if (!fs.existsSync(users_db_location)) {
+    fs.writeFileSync(users_db_location, "{}")
+} else {
+    try {
+        JSON.parse(fs.readFileSync(users_db_location, 'utf-8'))
+    } catch {
+        fs.writeFileSync(users_db_location, "{}")
+    }
+}
+
+/**
+ * Checks if a given user is allowed
+ * to make requests from a supplied IP address
+ * @param user - username string
+ * @param ip - connecting IP address
+ * @returns boolean
+ */
+export const user_is_authorized = async function user_is_authorized(user: string, ip: string) : Promise<boolean> {
+    let user_db = JSON.parse(fs.readFileSync(users_db_location, 'utf-8'))
+    if(user_db[user]) {
+        return user_db[user]["ip"] == ip;
+    }
+    return true;
+}
+
+/**
+ * creates a user
+ * @param user - username string
+ * @param ip - connecting IP address
+ * @returns void
+ */
+export const create_user = async function create_user(user: string, ip: string) : Promise<void> {
+    let user_db = JSON.parse(fs.readFileSync(users_db_location, 'utf-8'))
+    if(user_db[user]) {
+        throw new Error("Username already exists!");
+    }
+    user_db[user] = {
+        "ip" : ip,
+        "ka" : await networkClient.nodeClients[0].registerKeyAlias()
+    }
+    fs.writeFileSync(users_db_location, JSON.stringify(user_db));
+    return;
+}
+
+/**
+ * will get the key-alias string of a given user
+ * @param user 
+ * @returns string
+ */
+export const get_user_ka = async function get_user_ka(user:string) : Promise<string> {
+    let user_db = JSON.parse(fs.readFileSync(users_db_location, 'utf-8'))
+    if(!user_db[user]) {
+        throw new Error("User does not exist!");
+    }
+    return user_db[user]["ka"];
+}
+
+/**
+ * this function will get a list of users
+ * @returns list string[] of users
+ */
+export const list_users = async function list_users() : Promise<string[]> {
+    return Object.keys(JSON.parse(fs.readFileSync(users_db_location, 'utf-8')))
+}
