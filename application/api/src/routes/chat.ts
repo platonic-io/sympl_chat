5fca4ec09679a4fa03487fe807d2868cb54bfad3
David Bono <david.bono@symbiont.io>
2021-08-10 15:37:04 -0400

    added comments/fixed gitignore

--- application/api/src/routes/chat.ts
+++ application/api/src/routes/chat.ts
@@ -1,57 +1,51 @@
-/*
-import * as um from '../user-manager';
-import { chat } from '../assembly-wrapper';
-import { Room } from '@assembly/contract';
-import Koa, { Context }  from 'koa';
-import Router from 'koa-router';
-
-const assembly : Koa = new Koa();
-const assemblyRouter : Router = new Router();
-
-assemblyRouter.post('/create_room', async (ctx: Context) => {
-    ctx.body = await chat.createRoom(ctx.state.user, ctx.request.query.room_name);
-})
-
-
-export const assembly_api = assembly;
-/**/
 import Handlebars, { template } from 'handlebars';
 import { chat } from '../assembly-wrapper';
 import * as fs from 'fs';
 
+//register a helper to convert the js function names from
+//camelCase to lowercase and underscore
 Handlebars.registerHelper('underscore', (item : string) => {
     return item.toString().replace( /([A-Z])/g, "_$1").toLowerCase()
 })
 
+//generate the Handlebars template
 const assembly_routes = Handlebars.compile(fs.readFileSync(`${__dirname}/chat.ts.hbs`).toString())
 
+//create an object to pass to the template
 let function_meta = {"functions" : []}
 
+//get all the function names (from the imported model)
 const chat_function_names = Object.getOwnPropertyNames(Object.getPrototypeOf(chat))
     .filter(func_name => func_name != "teardown" && func_name != "constructor");
 
+//get the actual js code of the contract (this is so we can get the parameter names)
 const contract_definition = fs.readFileSync(`${__dirname}/../../@assembly/contract.js`).toString();
 
+//poplate the function meta data with json that looks like:
+/* 
+{
+    functions : [
+        {"name" : "myFunctionName", "parameters" : ["myFunctionParameters"...]}
+    ]    
+}
+*/
 for(let i in chat_function_names) {    
     let name = chat_function_names[i];
-    let regex = new RegExp(`async ${name}\\((.*)\\)`, 'g')
-    console.log(regex)
+    let regex = new RegExp(`async ${name}\\((.*)\\)`, 'g');
+    //push the new function meta object
     function_meta["functions"].push({
-        "name" : chat_function_names[i], 
+        "name" : chat_function_names[i],
+        //read the actual code and use a regex and .split() to extract the parameter names
          parameters : [...contract_definition.matchAll(regex)][0][1].split(',')
                         .filter(param => param != "keyAlias")
                         .map(Function.prototype.call, String.prototype.trim)
-    })
+    });
 }
 
-console.log(function_meta)
+//generate the template and write it to a file
 const template_chat_routes :string = assembly_routes(function_meta)
-
 fs.writeFileSync(`${__dirname}/generated_chat.ts`, template_chat_routes);
 
+//import from the newly created file and export the module
 import { assembly_api } from './generated_chat';
 export const chat_routes = assembly_api;
-
-
-
-
