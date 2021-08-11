08b9e8d748c0d2a89e77df7aa3920c2a800be848
David Bono <david.bono@symbiont.io>
2021-08-11 09:58:58 -0400

    now using Usage: sym generate [options]
    Codegen a bootstrap for the connected network published contracts
    
    Options:
      -c, --connection-file <connection_file_path>  network connection file (default: false)
      -n, --network-name <network_name>             local network name (default: "default")
      -i, --in <in>                                 Input templates directory (default: false)
      -o, --out <out>                               Output directory for bootstrap code (default: false)
      --overrides <overrides>                       Types override configuration file (default: false)
      --raw                                         Raw output, takes precedence over all other options (default: false)
      -h, --help                                    display help for command to make routes file

---
+++ application/api/src/routes/generated/chat.ts
@@ -0,0 +1,52 @@
+import { chat } from '../../assembly-wrapper';
+import {Context} from 'koa';
+import Router from 'koa-router';
+import * as um from '../../user-manager';
+
+const assemblyRouter : Router = new Router();
+
+assemblyRouter.post('/create_room', async (ctx: Context) => {
+    ctx.body = await chat.createRoom(ctx.state.user, 
+        ctx.state.room_name
+    );
+})
+assemblyRouter.post('/invite_to_room', async (ctx: Context) => {
+    ctx.body = await chat.inviteToRoom(ctx.state.user, 
+        ctx.state.room_channel, 
+        ctx.state.new_member
+    );
+})
+assemblyRouter.post('/send_message', async (ctx: Context) => {
+    ctx.body = await chat.sendMessage(ctx.state.user, 
+        ctx.state.room_channel, 
+        ctx.state.message
+    );
+})
+assemblyRouter.post('/remove_from_room', async (ctx: Context) => {
+    ctx.body = await chat.removeFromRoom(ctx.state.user, 
+        ctx.state.member_to_remove, 
+        ctx.state.room_channel
+    );
+})
+assemblyRouter.post('/get_rooms', async (ctx: Context) => {
+    ctx.body = await chat.getRooms(ctx.state.user
+    );
+})
+assemblyRouter.post('/restore_room', async (ctx: Context) => {
+    ctx.body = await chat.restoreRoom(ctx.state.user, 
+        ctx.state.room_channel
+    );
+})
+assemblyRouter.post('/get_messages', async (ctx: Context) => {
+    ctx.body = await chat.getMessages(ctx.state.user, 
+        ctx.state.room_channel
+    );
+})
+assemblyRouter.post('/delete_room', async (ctx: Context) => {
+    ctx.body = await chat.deleteRoom(ctx.state.user, 
+        ctx.state.room_channel
+    );
+})
+
+
+export const assembly_router : Router = assemblyRouter;
