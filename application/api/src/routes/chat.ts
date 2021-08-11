a2a83cc6cf1da7083bdc5fa027e06cd33220ee1e
David Bono <david.bono@symbiont.io>
2021-08-11 16:59:20 -0400

    fixed type issue with ctx.request.query.

--- application/api/src/routes/chat.ts
+++ application/api/src/routes/chat.ts
@@ -1,4 +1,4 @@
-import Koa, {Context} from 'koa';
+import Koa, {Context, Request} from 'koa';
 import { assembly_router } from './generated/chat';
 import * as um from '../user-manager';
 
@@ -8,7 +8,17 @@ assembly.use(async (ctx:Context, next: any) => {
     //swaps supplied 'usernames' for the associated key alias
     //from the users.json file
     for(let key in JSON.parse((JSON.stringify(ctx.request.query)))) {
-        ctx.state[key] = key.includes("owner") || key.includes("member") ? await um.get_ka_from_user(ctx.request.query[key]) : ctx.request.query[key];
+        //this is because the typescript type for request.query is string | string[]
+        //and we can't assume that it's going to be a string
+        let value : string = typeof(ctx.request.query[key]) === "string" ? 
+                                       ctx.request.query[key].toString() : 
+                                       ctx.request.query[key][0].toString();
+
+        //set the value of ctx.state[key] to the value of ctx.request.query[key]
+        //this makes the query parameters nice and easy for the auto generated
+        //routes file. 
+        ctx.state[key] = key.includes("owner") || key.includes("member") ? 
+                                        await um.get_ka_from_user(value) : value;
     }
     return next();
 })
