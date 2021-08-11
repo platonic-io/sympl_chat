0ed8d034288e919f8b756019c8763ae1db4f8b91
David Bono <david.bono@symbiont.io>
2021-08-11 09:58:58 -0400

    now using Usage: sym generate [options]
    Codegen a bootstrap for the connected network published contracts
    
    Options:
      -c, --connection-file <connection_file_path>  network connection file (default: false)
      -n, --network-name <network_name>             local network name (default: "default")
      -i, --in <in>                                 Input templates directory (default: false)
      -o, --out <out>                               Output directory for bootstrap code (default: false)
      --overrides <overrides>                       Types override configuration file (default: false)
      --raw                                         Raw output, takes precedence over all other options (default: false)
      -h, --help                                    display help for command to make routes file

--- application/api/src/routes/chat.ts
+++ application/api/src/routes/chat.ts
@@ -1,56 +1,29 @@
-import Handlebars, { template } from 'handlebars';
-import { chat } from '../assembly-wrapper';
-import Koa from 'koa';
-import * as fs from 'fs';
+import Koa, {Context} from 'koa';
+import { assembly_router } from './generated/chat';
+import * as um from '../user-manager';
 
-//register a helper to convert the js function names from
-//camelCase to lowercase and underscore
-Handlebars.registerHelper('underscore', (item : string) => {
-    return item.toString().replace( /([A-Z])/g, "_$1").toLowerCase()
+const assembly : Koa = new Koa();
+
+assembly.use(async (ctx:Context, next: any) => {
+    for(let key in JSON.parse((JSON.stringify(ctx.request.query)))) {
+        ctx.state[key] = key.includes("owner") || key.includes("member") ? await um.get_ka_from_user(ctx.request.query[key]) : ctx.request.query[key];
+    }
+    return next();
 })
 
-Handlebars.registerHelper('person', (item : string) => {
-    return item.toString().includes("member") || item.toString().includes("owner")
+assembly.use(async (ctx:Context, next: any) => {
+    await next();
+    let body_temp = ctx.body;
+    if(body_temp.room) {
+        body_temp.room["members"] = await Promise.all(body_temp.room["members"].map(
+            async (ka) => await um.get_user_from_ka(ka)
+        ))
+    }
+    ctx.body = body_temp;
+    console.log(ctx.body);
 })
 
-//generate the Handlebars template
-const assembly_routes = Handlebars.compile(fs.readFileSync(`${__dirname}/chat.ts.hbs`).toString())
+assembly.use(assembly_router.middleware());
 
-//create an object to pass to the template
-let function_meta = {"functions" : []}
 
-//get all the function names (from the imported model)
-const chat_function_names = Object.getOwnPropertyNames(Object.getPrototypeOf(chat))
-    .filter(func_name => func_name != "teardown" && func_name != "constructor");
-
-//get the actual js code of the contract (this is so we can get the parameter names)
-const contract_definition = fs.readFileSync(`${__dirname}/../../@assembly/contract.js`).toString();
-
-//poplate the function meta data with json that looks like:
-/* 
-{
-    functions : [
-        {"name" : "myFunctionName", "parameters" : ["myFunctionParameters"...]}
-    ]    
-}
-*/
-for(let i in chat_function_names) {    
-    let name = chat_function_names[i];
-    let regex = new RegExp(`async ${name}\\((.*)\\)`, 'g');
-    //push the new function meta object
-    function_meta["functions"].push({
-        "name" : chat_function_names[i],
-        //read the actual code and use a regex and .split() to extract the parameter names
-         parameters : [...contract_definition.matchAll(regex)][0][1].split(',')
-                        .filter(param => param != "keyAlias")
-                        .map(Function.prototype.call, String.prototype.trim)
-    });
-}
-
-//generate the template and write it to a file
-const template_chat_routes :string = assembly_routes(function_meta)
-fs.writeFileSync(`${__dirname}/generated_chat.ts`, template_chat_routes);
-
-//import from the newly created file and export the modules
-import { assembly_api } from './generated_chat';
-export const chat_routes : Koa = assembly_api;
+export const chat_routes : Koa = assembly;
