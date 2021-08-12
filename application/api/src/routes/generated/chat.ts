c78e5afb50c006723b61ed9c768edbe813dfebee
David Bono <david.bono@symbiont.io>
2021-08-12 15:40:36 -0400

    fixed template

--- application/api/src/routes/generated/chat.ts
+++ application/api/src/routes/generated/chat.ts
@@ -2,11 +2,20 @@ import { chat } from '../../assembly-wrapper';
 import {Context} from 'koa';
 import Router from 'koa-router';
 
+/* 
+ *  --- This code was auto generated from chat.ts.hbs ---
+ * /
 const assemblyRouter : Router = new Router();
 
 assemblyRouter.post('/create_room', async (ctx: Context) => {
     ctx.body = await chat.createRoom(ctx.state.user, 
         ctx.state.room_name
+    );
+})
+assemblyRouter.post('/demote_owner', async (ctx: Context) => {
+    ctx.body = await chat.demoteOwner(ctx.state.user, 
+        ctx.state.room_channel, 
+        ctx.state.owner
     );
 })
 assemblyRouter.post('/invite_to_room', async (ctx: Context) => {
@@ -19,6 +28,12 @@ assemblyRouter.post('/send_message', async (ctx: Context) => {
     ctx.body = await chat.sendMessage(ctx.state.user, 
         ctx.state.room_channel, 
         ctx.state.message
+    );
+})
+assemblyRouter.post('/promote_to_owner', async (ctx: Context) => {
+    ctx.body = await chat.promoteToOwner(ctx.state.user, 
+        ctx.state.room_channel, 
+        ctx.state.member
     );
 })
 assemblyRouter.post('/remove_from_room', async (ctx: Context) => {
