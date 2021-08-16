f678a75904349761e3264b732608442ae3123a04
David Bono <david.bono@symbiont.io>
2021-08-16 10:45:12 -0400

    display messages/rooms

--- application/ui/assets/js/api.js
+++ application/ui/assets/js/api.js
@@ -7,7 +7,7 @@
 })*/
 
 const api_base = "/api"
-async function call_api(method, api_method, parameters, headers, data) {
+async function call_api(method, api_method, parameters={}, headers={}, data="") {
     url = `${api_base}/${api_method}`
     if(Object.keys(parameters).length >0) {
         url += '?';
@@ -17,12 +17,16 @@ async function call_api(method, api_method, parameters, headers, data) {
         }
         url += params_temp.join('&')
     }
+    if( (!localStorage.username) && method !== "create_user") {
+        throw new Error("Error, no username created yet")
+    }
+    headers["username"] = localStorage.username;
+
     return fetch(url, {
         method: method,
         headers: headers,
-        data: data
+        body: data
     }).then((res) => {
         return res.json();
     })
-    
 }
