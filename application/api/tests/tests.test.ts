61edab31ca32c78be4047cf507b0f306133d86e3
David Bono <david.bono@symbiont.io>
2021-08-20 14:14:20 -0400

    two tests

--- application/api/tests/tests.test.ts
+++ application/api/tests/tests.test.ts
@@ -1,8 +1,14 @@
 import * as um from '../src/user-manager'
 import {expect, test, it, describe} from '@jest/globals';
 
-describe('firsttest', () => {
-    it("Create_User/Get_User", async () => {
+describe('key-alias-check', () => {
+    it("tests-key-alias-function", async () => {
         expect(await um.is_key_alias("jahlksdfjhalksdf")).toBe(false);
+    })
+})
+
+describe('key-alias-check', () => {
+    it("tests-key-alias-function", async () => {
+        expect(await um.is_key_alias("KA-2523277240480267")).toBe(true);
     })
 })
