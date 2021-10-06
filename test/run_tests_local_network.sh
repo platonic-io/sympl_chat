450eaee70183e0787846c51b446f201ecdb6daeb
David Bono <david.bono@symbiont.io>
2021-10-06 13:50:54 -0400

    skip consistency local-network check

--- test/run_tests_local_network.sh
+++ test/run_tests_local_network.sh
@@ -7,12 +7,5 @@ source ./env.sh
 
 sym local-network start --nodes 4
 
-if ! timeout 30s bash -c 'until curl --silent http://localhost:8888/api/v1; do sleep 1; done'; then
-    echo "Timed out waiting for sandbox to start"
-    exit 1
-fi
-
 DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/dev-network/default/network-config.json"
-
-sym network publish-contract -d ../
-pytest ../test/ --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../
+pytest ../test/ --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --skip-consistency-check
