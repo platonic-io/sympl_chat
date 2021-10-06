4a5228d8f16d3f3216f6f19a2ce5384800d46320
David Bono <david.bono@symbiont.io>
2021-10-06 13:53:21 -0400

    install pytest

--- test/run_tests_local_network.sh
+++ test/run_tests_local_network.sh
@@ -7,5 +7,8 @@ source ./env.sh
 
 sym local-network start --nodes 4
 
+pip3 install --upgrade pip
+pip install $HOME/.symbiont/versions/current/pytest/pytest_assembly-1.0.3-py3-none-any.whl
+
 DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/dev-network/default/network-config.json"
 pytest ../test/ --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --skip-consistency-check
