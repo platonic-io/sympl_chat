fc989695e5f33c7c72d5fcd0a80548df0745b54a
David Bono <david.bono@symbiont.io>
2021-10-08 16:21:07 -0400

    run consistency check

--- test/run_tests_local_network.sh
+++ test/run_tests_local_network.sh
@@ -11,4 +11,4 @@ pip3 install --upgrade pip
 pip install $HOME/.symbiont/versions/current/pytest/pytest_assembly-1.0.3-py3-none-any.whl
 
 DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/dev-network/default/network-config.json"
-pytest ../test/ --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --skip-consistency-check
+pytest ../test/ --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
