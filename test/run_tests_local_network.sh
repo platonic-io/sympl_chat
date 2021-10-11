dee432840a22c3e01b9279c513c633eec091f812
David Bono <david.bono@symbiont.io>
2021-10-11 13:34:35 -0400

    temporary only run model tests

--- test/run_tests_local_network.sh
+++ test/run_tests_local_network.sh
@@ -11,4 +11,4 @@ pip3 install --upgrade pip
 pip3 install $HOME/.symbiont/versions/current/pytest/pytest_assembly-1.0.3-py3-none-any.whl
 
 DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/dev-network/default/network-config.json"
-pytest ../test/ --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
+pytest ../test/chat_8_3_0_0_model_test.py --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
