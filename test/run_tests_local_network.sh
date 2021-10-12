e4ce36aa7b19d9c07f46af1c9ef42e252bd9e0d7
symbiont-david-bono <87322808+symbiont-david-bono@users.noreply.github.com>
2021-10-12 09:17:32 -0400

    Fix/db/pytest local network consistency check (#14)
    * run consistency check
    
    * remove installing pytest from pypi
    
    * python3 pytest
    
    * force pytest3
    
    * removed excess newlines
    
    * use pytest
    
    * temporary only run model tests
    
    * run each test separately

--- test/run_tests_local_network.sh
+++ test/run_tests_local_network.sh
@@ -8,7 +8,10 @@ source ./env.sh
 sym local-network start --nodes 4
 
 pip3 install --upgrade pip
-pip install $HOME/.symbiont/versions/current/pytest/pytest_assembly-1.0.3-py3-none-any.whl
+pip3 install $HOME/.symbiont/versions/current/pytest/pytest_assembly-1.0.3-py3-none-any.whl
 
 DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/dev-network/default/network-config.json"
-pytest ../test/ --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --skip-consistency-check
+pytest ../test/chat_8_3_0_0_model_test.py --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
+pytest ../test/chat_8_3_0_0_coverage_test.py --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
+pytest ../test/chat_8_3_0_0_demo_test.py --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
+pytest ../test/chat_8_3_0_0_events_test.py --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
