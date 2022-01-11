3e227294e42506e881f866698f4ba6fa21fe612c
symbiont-david-bono <87322808+symbiont-david-bono@users.noreply.github.com>
2022-01-11 10:02:59 -0500

    change location of where pytest plugin is installed from (#21)

--- test/run_tests_local_network.sh
+++ test/run_tests_local_network.sh
@@ -8,7 +8,7 @@ source ./env.sh
 sym local-network start --nodes 4
 
 pip3 install --upgrade pip
-pip3 install $HOME/.symbiont/versions/current/pytest/pytest_assembly-1.0.3-py3-none-any.whl
+pip3 install symbiont-io.pytest-assembly
 
 DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/dev-network/default/network-config.json"
 pytest ../test/chat_8_3_0_0_model_test.py --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
--- test/run_tests_sandbox.sh
+++ test/run_tests_sandbox.sh
@@ -17,6 +17,6 @@ DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/mock-network/default/networ
 sym network publish-contract -d ../
 
 pip3 install --upgrade pip
-pip3 install $HOME/.symbiont/versions/current/pytest/pytest_assembly-1.0.3-py3-none-any.whl
+pip3 install symbiont-io.pytest-assembly
 
 pytest ../test/ --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../
