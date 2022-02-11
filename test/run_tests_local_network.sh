949fb5f0365ce31e5c12ac5a5d15f9e0f77cb30b
symbiont-tom-holder <60674274+symbiont-tom-holder@users.noreply.github.com>
2022-02-11 17:07:31 -0500

    Upgrade sympl_chat to be compatible with 2.0.4 pytest (#28)
    * Upgrade sympl_chat to be compatible with 2.0.4 pytest
    
    * Upgrade to 2.0.4.dev1

--- test/run_tests_local_network.sh
+++ test/run_tests_local_network.sh
@@ -8,7 +8,7 @@ source ./env.sh
 sym local-network start --nodes 4
 
 pip3 install --upgrade pip
-pip3 install symbiont-io.pytest-assembly
+pip3 install symbiont-io.pytest-assembly==2.0.4.dev1
 
 DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/dev-network/default/network-config.json"
 pytest ../test/chat_9_3_0_0_model_test.py --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
