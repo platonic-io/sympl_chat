f8521f8c0ae5ef8c4bea7679708d4767bda30d45
symbiont-tom-holder <60674274+symbiont-tom-holder@users.noreply.github.com>
2022-03-23 12:52:24 -0400

    Remove tx (#31)
    * move key rotation
    
    * Include assembly-client in the build

--- test/run_tests_local_network.sh
+++ test/run_tests_local_network.sh
@@ -6,7 +6,7 @@ echo "--- Contracts Test Workflow"
 
 sym local-network start --nodes 4
 
-pip3 install symbiont-io.pytest-assembly==2.0.4.dev1
+pip3 install symbiont-io.pytest-assembly==2.0.4.dev9
 
 DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/dev-network/default/network-config.json"
 pytest ../test/chat_9_3_0_0_model_test.py --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ --baseline -p no:pytest-mp
